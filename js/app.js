61ab7d917555555e5283124855f27d93b595c22f
rokib2000 <rokib0w1@gmail.com>
2022-09-05 01:28:21 +0600

    set all data HTML page by JS

--- js/app.js
+++ js/app.js
@@ -1,9 +1,22 @@
 const loadData = (city) => {
   const tempAPI = "a9c7845ea89c6f6637addbaa87ca623d";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${tempAPI}`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${tempAPI}&units=metric`;
   fetch(url)
     .then((res) => res.json())
-    .then((data) => console.log(data));
+    .then((data) => displayData(data));
+};
+
+const displayData = (data) => {
+  console.log(data);
+  setInnerTextById("city", data.name);
+  setInnerTextById("temperature", data.main.temp);
+  setInnerTextById("condition", data.weather[0].main);
+  //   console.log(data.name);
+};
+
+const setInnerTextById = (id, data) => {
+  const temperature = document.getElementById(id);
+  temperature.innerText = data;
 };
 
 loadData("Dhaka");
